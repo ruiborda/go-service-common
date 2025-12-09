4bef4d648bf750b5a4062b78db6886f19ee62d41
RuiBorda <bordarui@gmail.com>
2025-12-09 13:56:51 -0500

    fix: error provider

--- dto/Response.go
+++ dto/Response.go
@@ -1,5 +1,9 @@
 package dto
 
+type ResponseErrorProvider interface {
+	GetErrors() *[]*Error
+}
+
 type Response[T any] struct {
 	Status  int       `json:"status"`
 	Message *string   `json:"message,omitempty"`
@@ -15,11 +19,36 @@ func ResponseBuilder[T any](status int, body T) *Response[T] {
 	}
 }
 
+func (r *Response[T]) GetErrors() *[]*Error {
+	return r.Errors
+}
+
+func (r *Response[T]) MergeErrors(other ResponseErrorProvider) *Response[T] {
+	if other == nil || other.GetErrors() == nil {
+		return r
+	}
+	return r.AddErrors(other.GetErrors())
+}
+
 func (r *Response[T]) AddError(err *Error) *Response[T] {
 	if err == nil {
 		return r
 	}
+	if r.Errors == nil {
+		r.Errors = &[]*Error{}
+	}
 	*r.Errors = append(*r.Errors, err)
+	return r
+}
+
+func (r *Response[T]) AddErrors(errors *[]*Error) *Response[T] {
+	if errors == nil {
+		return r
+	}
+	if r.Errors == nil {
+		r.Errors = &[]*Error{}
+	}
+	*r.Errors = append(*r.Errors, *errors...)
 	return r
 }
 
@@ -38,14 +67,6 @@ func (r *Response[T]) SetBody(body T) *Response[T] {
 	return r
 }
 
-func (r *Response[T]) AddErrors(errors *[]*Error) *Response[T] {
-	if errors == nil {
-		return r
-	}
-	*r.Errors = append(*r.Errors, *errors...)
-	return r
-}
-
 func (r *Response[T]) SetErrors(errors *[]*Error) *Response[T] {
 	if errors == nil {
 		return r
@@ -56,11 +77,4 @@ func (r *Response[T]) SetErrors(errors *[]*Error) *Response[T] {
 
 func (r *Response[T]) HasErrors() bool {
 	return r.Errors != nil && len(*r.Errors) > 0
-}
-
-func (r *Response[T]) MergeErrors(otherResponse *Response[T]) *Response[T] {
-	if otherResponse == nil || otherResponse.Errors == nil {
-		return r
-	}
-	return r.AddErrors(otherResponse.Errors)
 }
